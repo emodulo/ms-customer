0ae33bc363889de64bb488d63119d967155638d4
Kreverson <kreversonjulho@gmail.com>
2025-06-06 15:47:11 -0300

    Added docker and docker compose configuration

--- src/main/java/br/com/emodulo/ms_customer/MsCustomerApplication.java
+++ src/main/java/br/com/emodulo/ms_customer/MsCustomerApplication.java
@@ -2,6 +2,7 @@ package br.com.emodulo.ms_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class MsCustomerApplication {
